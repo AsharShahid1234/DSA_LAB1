2a755616b26d26e02480471d974163477069d965
Ash_Shahid_22 <106060774+AsharShahid1234@users.noreply.github.com>
2024-11-20 15:00:24 +0500

    LAB_5

---
+++ LAB_5.java
@@ -0,0 +1,134 @@
+# TASK_1
+
+  package DSA_LABS_TASK;
+
+public class Lab5_Task1 {
+    public static void main(String[] args) {
+        int[] arr = {53, 15, 18, 15, 28, 17, 1}; 
+        selectionSort(arr);
+    }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+            System.out.print("Iteration " + (i + 1) + ": ");
+            for (int k = 0; k < n; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+# TASK_2
+
+  package DSA_LABS_TASK;
+import java.util.Scanner;
+public class Lab5_Task2  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        System.out.println("Enter 10 numbers:");
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+    }
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) { 
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            System.out.print("Iteration " + (i + 1) + ": ");
+            for (int k = 0; k < n; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+# TASK_3
+
+  package DSA_LABS_TASK;
+public class Lab5_Task3 {
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Given Array:");
+        printArray(arr);
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid); 
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right); 
+            printArray(arr);
+        }}
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
+  
